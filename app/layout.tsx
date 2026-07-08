a534d77b5b40ed30052b67366409c2cfd2f9d347
Rohit Waman <rohitwaman614@gmail.com>
2026-07-08 16:12:47 +0530

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -11,9 +12,6 @@ export const metadata: Metadata = {
   title: "The Infinex Technologies",
   description:
     "Software Development | AI Solutions | Web Development | Mobile Apps",
-  verification: {
-    google: "BSdN9770FDQEcYbTgDKmgVBY7Ym1CE9pZ85okYbWzX8",
-  },
 };
 
 export default function RootLayout({
@@ -26,6 +24,8 @@ export default function RootLayout({
       <body className={poppins.className}>
         {children}
       </body>
+
+      <GoogleAnalytics gaId="G-GD396GKZYS" />
     </html>
   );
 }
